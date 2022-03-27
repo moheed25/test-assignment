03e299ccaf26d99e90f11e11ea3cf15060a8e2cc
moheed25 <moheed25@mohedaziz.com>
2022-03-27 12:03:42 -0700

    test assignment complete

---
+++ main.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+void main() {
+//QUESTION # 1:
+//    print('Enter N');
+//  // var num = 5;
+//   int N = int.parse(stdin.readLineSync()!); //(stdin.readLineSync()!);
+
+//   int result = 1;
+//   for (int i = 1; i <= N; i++) {
+//     result *= i;
+//   }
+
+//   print('Factorial of $N');
+//   print(result);
+// }
+
+//QUESTION # 2:
+
+  //  int i, j, numbers, n = 4;
+
+  //  for(i=0;i<n;i++){
+  //   numbers = 1;
+  //    for(j = 0; j<=i;j++)
+  //    {
+  //      stdout.write('${numbers} ');
+  //      numbers++;
+  //    }
+  //    stdout.writeln();
+  // }
+
+// QUESTION # 3:
+
+// main(List<String> args) {
+//   var sum = 0;
+//   var given_list = [1,5,9,13,17,21,25,29,33,37];
+
+//   for (var i = 0; i < given_list.length; i++) {
+//     sum += given_list[i];
+//   }
+
+//   print("Sum : ${sum}");
+//QUESTION # 6:
+//   String character = (stdin.readLineSync()!);
+//   print("hello , ${character}");
+//   if ((character == 65 &&  character == 90) ||
+//       (character == 97 && character == 122)) {
+//     print("alphabet");
+//   } else if(character == 48 && character == 57) {
+//     print("digit");
+//   }else{
+//     print("special character");
+//   }
+// }
+}
